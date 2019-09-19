ca4d201d8e03c7bcfd5802a8c4509b1e07c32a4e
koyamanX <ckoyama.1996@gmail.com>
2019-09-20 03:53:14 +0900

    supress newline

--- bin/decurl.py
+++ bin/decurl.py
@@ -4,6 +4,6 @@ import urllib.parse
 import sys
 def main():
     with open(sys.argv[1]) as f: 
-        print(urllib.parse.unquote(f.readline()))
+        print(urllib.parse.unquote(f.readline()).rstrip(), end="")
 if __name__ == '__main__':
     main()
