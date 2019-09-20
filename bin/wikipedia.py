2a914d77fa4dc42ccf51024383177f8d77d5d647
koyamanX <ckoyama.1996@gmail.com>
2019-09-21 04:52:23 +0900

    simplify wikipedia scraping program

---
+++ bin/wikipedia.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python2
+#encoding:utf-8
+from __future__ import print_function
+from bs4 import BeautifulSoup
+import urllib2
+import sys
+    
+def main():
+    argc = len(sys.argv)
+    argv = sys.argv
+    if argc != 2:
+        sys.exit(1)
+    url = "https://ja.wikipedia.org/wiki/" + sys.argv[1].encode('utf-8')
+    html = urllib2.urlopen(url).read()
+    soup = BeautifulSoup(html, 'html.parser')
+    ps = soup.find_all('p')
+    for p in ps:
+        print(p.get_text().encode('utf-8'), end=',')
+if __name__ == '__main__':
+    main()
+
